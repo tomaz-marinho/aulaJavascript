fc0db4a1a6ff16a0ad9f9f3e64ac47779dfca1fd
Tomaz Gomes Marinho <156114958+tomaz-marinho@users.noreply.github.com>
2025-01-02 09:08:41 -0300

    estudando na empresa

---
+++ 03-Praticando/Exercicio3.js
@@ -0,0 +1,20 @@
+/* 
+3) Elabore um algoritimo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta
+ e a escolha da condição de pagamento.
+Utilize os codigos da tanela a seguir para ler qual a condição de pagamento escolhida e efetuar o calculo
+adequado.
+
+Código Condioção de pagamento:
+
+- À vista débito, recebe 10% de desconto;
+- à vista no dinheiro ou pix, recebe 15% de desconto;
+- Em duas vezes, preço normal de etiqueta sem juros;
+- Acima de duas vezes, preço normal de etiqueta mais juros
+de 10%;
+
+*/
+
+const precoEtiqueta = 100;
+const debito = (precoEtiqueta + (precoEtiqueta*0.10));
+
+console log ();
